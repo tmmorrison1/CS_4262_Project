40867239500418659be7399c9a2b81c80149afa6
Mark Weinstein <mweinstein91@yahoo.com>
2019-12-01 20:41:21 -0600

    added differentials

--- LeagueInfo.py
+++ LeagueInfo.py
@@ -4,10 +4,61 @@ import matplotlib as plt
 import sys
 
 name = '_min100'
+def team_aggregate_diff(matches, save=False):
+    global name
+    matches['kill_diff_blue'] = matches['bKills'] - matches['rKills']
+    matches['kill_diff_red'] = -1 * matches['kill_diff_blue']
+    matches['tower_diff_blue'] = matches['bTowers'] - matches['rTowers']
+    matches['tower_diff_red'] = -1 * matches['tower_diff_blue']
+    matches['inhib_diff_blue'] = matches['bInhibs'] - matches['rInhibs']
+    matches['inhib_diff_red'] = -1 * matches['inhib_diff_blue']
+    matches['dragon_diff_blue']= matches['bDragons'] - matches['rDragons']
+    matches['dragon_diff_red'] = -1 * matches['dragon_diff_blue']
+
+    critical_cols_blue = matches[
+        ['blueTeamTag', 'golddiff', 'kill_diff_blue', 'tower_diff_blue', 'inhib_diff_blue', 'dragon_diff_blue', 'bResult', 'Address']]
+    critical_cols_red = matches[
+        ['redTeamTag', 'golddiff', 'kill_diff_red', 'tower_diff_red', 'inhib_diff_red', 'dragon_diff_red', 'rResult', 'Address']]
+
+    critical_cols_blue['summedGoldDiff'] = critical_cols_blue['golddiff'].apply(lambda x: sum(eval(x)))
+    critical_cols_red['summedGoldDiff'] = critical_cols_red['golddiff'].apply(lambda x: sum(eval(x)))
+    critical_cols_blue['kills'] = critical_cols_blue['kill_diff_blue'].apply(lambda x: len(eval(x)))
+    critical_cols_red['kills'] = critical_cols_red['kill_diff_red'].apply(lambda x: len(eval(x)))
+    critical_cols_blue['towers'] = critical_cols_blue['tower_diff_blue'].apply(lambda x: len(eval(x)))
+    critical_cols_red['towers'] = critical_cols_red['tower_diff_red'].apply(lambda x: len(eval(x)))
+    critical_cols_blue['inhibs'] = critical_cols_blue['inhib_diff_blue'].apply(lambda x: len(eval(x)))
+    critical_cols_red['inhibs'] = critical_cols_red['inhib_diff_red'].apply(lambda x: len(eval(x)))
+    critical_cols_blue['dragons'] = critical_cols_blue['dragon_diff_blue'].apply(lambda x: len(eval(x)))
+    critical_cols_red['dragons'] = critical_cols_red['dragon_diff_red'].apply(lambda x: len(eval(x)))
+
+    games_played_blue = critical_cols_blue.groupby('blueTeamTag').size().sort_index()
+    games_played_red = critical_cols_red.groupby('redTeamTag').size().sort_index()
+    blues_avg = critical_cols_blue.groupby('blueTeamTag').agg('sum').sort_index()
+    blues_avg['count'] = games_played_blue
+    # blues_avg.to_csv('blue_avg.csv')
+    red_avg = critical_cols_red.groupby('redTeamTag').agg('sum').sort_index()
+    red_avg['count'] = games_played_red
+    # red_avg.to_csv('red_avg.csv')
+
+    red_blue = red_avg.join(blues_avg, lsuffix='_red', rsuffix='_blue')
+    red_blue['games'] = red_blue['count_red'] + red_blue['count_blue']
+    red_blue['win_pct'] = (red_blue['rResult'] + red_blue['bResult']) / red_blue['games']
+    red_blue['goldDiff'] = (-red_blue['summedGoldDiff_red'] + red_blue['summedGoldDiff_blue']) / red_blue['games']
+    red_blue['kills'] = (red_blue['kills_red'] + red_blue['kills_blue']) / red_blue['games']
+    red_blue['towers'] = (red_blue['towers_red'] + red_blue['towers_blue']) / red_blue['games']
+    red_blue['inhibs'] = (red_blue['inhibs_red'] + red_blue['inhibs_blue']) / red_blue['games']
+    red_blue['dragons'] = (red_blue['dragons_red'] + red_blue['dragons_blue']) / red_blue['games']
+    total_agg = red_blue[['games', 'win_pct', 'goldDiff', 'kills', 'towers', 'inhibs', 'dragons']]
+
+    if save:
+        total_agg.rename_axis("team").to_csv('data/total_agg_diff' + name + '.csv')
+
+    return total_agg
+
 
 def team_aggregate(matches, save=False):
     global name
-    
+
     critical_cols_blue = matches[['blueTeamTag', 'golddiff', 'bKills', 'bTowers', 'bInhibs', 'bDragons', 'bResult', 'Address']]
     critical_cols_red = matches[['redTeamTag', 'golddiff',  'rKills', 'rTowers', 'rInhibs', 'rDragons','rResult', 'Address']]
 
